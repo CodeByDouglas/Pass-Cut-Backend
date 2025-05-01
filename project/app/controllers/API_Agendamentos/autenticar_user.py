092d2891b5c6ed1a9bc4e2289474b55ac57bc3d0
DougDev <douglas.ofc.2004@gmail.com>
2025-05-01 03:40:32 +0000

    `feat(auth):` valida token de autenticação e estabelecimento no header

--- project/app/controllers/API_Agendamentos/autenticar_user.py
+++ project/app/controllers/API_Agendamentos/autenticar_user.py
@@ -4,8 +4,18 @@ autenticar_user_bp = Blueprint('autenticar_user', __name__, url_prefix='/api/aut
 
 @autenticar_user_bp.route('', methods=['POST'])
 def autenticar_user():
-    # Você pode adicionar aqui a lógica de autenticação no futuro.
-    return jsonify({
-        "status": "success",
-        "message": "Requisição POST recebida com sucesso."
-    }), 200
+    # Recupera os parâmetros do cabeçalho
+    authorization = request.headers.get('Authorization')
+    token_estabelecimento = request.headers.get('token-estabelecimento')
+    
+    # Verifica se ambos os parâmetros estão presentes e não são vazios
+    if authorization and token_estabelecimento:
+        return jsonify({
+            "status": "success",
+            "message": "Requisição POST recebida com sucesso 2."
+        }), 200
+    else:
+        return jsonify({
+            "status": "error",
+            "message": "Erro de autenticação"
+        }), 401
