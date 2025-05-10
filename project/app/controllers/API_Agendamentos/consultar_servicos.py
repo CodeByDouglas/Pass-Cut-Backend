32ef288cf572ec9e9abda221c71d0ee23bb30e7a
DougDev <douglas.ofc.2004@gmail.com>
2025-05-10 03:18:21 +0000

    feat(consultar_servicos): validar token de usuário

--- project/app/controllers/API_Agendamentos/consultar_servicos.py
+++ project/app/controllers/API_Agendamentos/consultar_servicos.py
@@ -3,6 +3,7 @@ from ...services.Services_Agendamentos.Verificacao_Dados.Verificar_Token_Fernet 
 from ...services.Services_Agendamentos.Verificacao_Dados.Verificar_Token_JWT import verificar_token_jwt
 from ...services.Services_Agendamentos.Autenticacao_Tokens.Validar_Token_consultar_servicos import validar_token_consultar_servico
 from ...services.Services_Agendamentos.Autenticacao_Tokens.Validar_Token_ID_estebelecimento import validar_token_id_estabelecimento
+from ...services.Services_Agendamentos.Autenticacao_Tokens.Validar_Token_ID_user import validar_token_id_user
 
 consultar_servicos_bp = Blueprint('consultar_servicos', __name__)
 
@@ -20,9 +21,15 @@ def consultar_servicos():
             if validar_token_consultar_servico(auth):
                 valid_est, estabelecimento_id = validar_token_id_estabelecimento(token_estabelecimento)
                 if valid_est:
-                    return jsonify({
-                        "message": "Requisição bem sucedida",
-                        "estabelecimento_id": estabelecimento_id
-                    }), 200
+                    valid_user = validar_token_id_user(estabelecimento_id, token_user)
+                    if valid_user:
+                        _, user_id = valid_user
+                        return jsonify({
+                            "message": "Requisição bem sucedida",
+                            "estabelecimento_id": estabelecimento_id,
+                            "user_id": user_id
+                        }), 200
+                    else:
+                        return jsonify({"erro": "Autenticação falhou"}), 401
                     
     return jsonify({"erro": "Autenticação falhou"}), 401
