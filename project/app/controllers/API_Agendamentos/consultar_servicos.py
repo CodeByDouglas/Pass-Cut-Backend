8b276e170963dd77d2eb81339a07d25773b5acc4
DougDev <douglas.ofc.2004@gmail.com>
2025-05-10 02:11:57 +0000

    feat(api): add header token validation for consultar_servicos_bp endpoint

--- project/app/controllers/API_Agendamentos/consultar_servicos.py
+++ project/app/controllers/API_Agendamentos/consultar_servicos.py
@@ -4,5 +4,11 @@ consultar_servicos_bp = Blueprint('consultar_servicos', __name__)
 
 @consultar_servicos_bp.route('/consultar-servicos', methods=['POST'])
 def consultar_servicos():
-    # Lógica para tratar a requisição POST pode ser adicionada aqui
+    auth = request.headers.get('auth')
+    token_estabelecimento = request.headers.get('token-estabelecimento')
+    token_user = request.headers.get('token-user')
+    
+    if not auth or not token_estabelecimento or not token_user:
+        return jsonify({"erro": "Autenticação falhou"}), 401
+    
     return jsonify({"message": "Requisição bem sucedida"}), 200
